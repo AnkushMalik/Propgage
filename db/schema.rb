e5c976b4aea5091d46db51ce1edfaf494f302de3
AnkushMalik <ankushmalik631@gmail.com>
2018-02-03 20:43:55 +0530

    Add name and phnum to User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180203124758) do
+ActiveRecord::Schema.define(version: 20180203145101) do
 
   create_table "bids", force: :cascade do |t|
     t.text "description"
@@ -55,6 +55,8 @@ ActiveRecord::Schema.define(version: 20180203124758) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "phnum"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
