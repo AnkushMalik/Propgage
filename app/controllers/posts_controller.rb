732c6f2a5940bd757c2f69e5cb07b7cf7f94f36f
AnkushMalik <ankushmalik631@gmail.com>
2018-02-03 17:38:31 +0530

    Add initial post functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+
 
   # GET /posts
   # GET /posts.json
@@ -24,7 +26,7 @@ class PostsController < ApplicationController
   # POST /posts
   # POST /posts.json
   def create
-    @post = Post.new(post_params)
+    @post = current_user.posts.new(post_params)
 
     respond_to do |format|
       if @post.save
@@ -69,6 +71,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :description, :tag_id, :user_id, :status)
+      params.require(:post).permit(:title, :description, :tag_id)
     end
 end
