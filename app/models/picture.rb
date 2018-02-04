5bc76d8194c8e3419b51505bda6c205e766ac6bb
AnkushMalik <ankushmalik631@gmail.com>
2018-02-04 08:18:01 +0530

    Add carousel properly in feeds

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,6 +1,6 @@
 class Picture < ApplicationRecord
   belongs_to :post
-  has_attached_file :pic, styles: { medium: "300x300>",sqimg: "400x400#", thumb: "100x100>" }, default_url: "imported/no_pic.png",
+  has_attached_file :pic, styles: {large: '600x400>'}, default_url: "imported/no_pic.png",
                     path: ":rails_root/public/pics/:id/:filename",
                     url: "/pics/:id/:filename"
 	validates_attachment_content_type :pic, content_type: /\Aimage\/.*\z/
